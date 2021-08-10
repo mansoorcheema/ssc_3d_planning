109f6ea719986253c91fa0181dfaa9133c4fb305
mansoor <mansoor.cheema@tum.de>
2021-08-10 15:19:36 +0200

    updated with voxel merging template specializations for upsampling predictions

--- ssc_mapping/src/ssc.cpp
+++ ssc_mapping/src/ssc.cpp
@@ -5,26 +5,27 @@
 #include <voxblox/core/common.h>
 #include <voxblox/core/layer.h>
 #include <voxblox/core/voxel.h>
+#include <voxblox/integrator/merge_integration.h>
+#include <voxblox/utils/color_maps.h>
 
 #include "std_msgs/Float32MultiArray.h"
 #include <tf/transform_listener.h>
 
 #include <ssc_msgs/SSCGrid.h>
 
-
 namespace voxblox {
 
 struct SSCOccupancyVoxel {
     float probability_log = 0.0f;
     bool observed = false;
-    int label;
-    float class_confidence;
+    int label = -1;
+    float class_confidence = 0.0f;
 };
 
 class SSCMap {
    public:
     struct Config {
-        FloatingPoint ssc_voxel_size = 0.3;
+        FloatingPoint ssc_voxel_size = 0.2;
         size_t ssc_voxels_per_side = 16u;
 
         std::string print() const;
@@ -57,45 +58,73 @@ class SSCServer {
     }
 
     void sscCallback(const ssc_msgs::SSCGrid::ConstPtr& msg) {
-
-        std::cout<<"frame:"<<msg->frame<<std::endl;
-        std::cout<<"Origin_x:"<<msg->origin_x<<std::endl;
-        std::cout<<"Origin_y:"<<msg->origin_y<<std::endl;
-        std::cout<<"Origin_z:"<<msg->origin_z<<std::endl;
-
-        // std::cout<<"depth:"<<msg->depth<<std::endl;
-        // std::cout<<"width:"<<msg->width<<std::endl;
-        // std::cout<<"height:"<<msg->height<<std::endl;
+        std::cout << "frame:" << msg->frame << std::endl;
+        std::cout << "Origin_x:" << msg->origin_x << std::endl;
+        std::cout << "Origin_y:" << msg->origin_y << std::endl;
+        std::cout << "Origin_z:" << msg->origin_z << std::endl;
 
         // todo - resize voxels to full size? Resize voxel grid from 64x36x64 to 240x144x240
+        // todo - update the  ssc_msgs::SSCGrid to contain the scale instead of hard coding
+        Layer<SSCOccupancyVoxel> temp_layer(ssc_map_->getSSCLayerPtr()->voxel_size() * 4,
+                                            ssc_map_->getSSCLayerPtr()->block_size() / 4);
 
         // completions are in odometry frame
+        // Note: numpy flattens an array (x,y,z) such that
+        // X
+        //  \
+        //   \
+        //    + ------------------> Z
+        //    |
+        //    |
+        //    v
+        //    Y axis
+        // Input from simulation are X,Y,Z. X forward, Z up and Y left.
+        // Its converted to grid coordinate system for
+        // input to network as Y,Z,X and similarly
+        // Network does predictions in Y,Z,X coordinates.
+        // predictions in Y,Z,X are flattened using numpy (for sending as array)
+        // These flattened predictions have are formated as:
+        // first 240 would have z from 0 to 239, y=0, z=0
+        // and similarly next 240 would have y=1,x=0
+        // and so on. if we read here in numpy way way then output would
+        // be in same Y,Z,X form.
+        // Solution:
+        // Either convert here from Y,Z,X to X,Y,Z and load in the same format
+        // as numpy saved
+        // Or  convert Y,Z,X -> X,Y,Z before sending and send transpose
+        // of X,Y,Z from Numpy and load here with the x being fastest axis.
         for (size_t x = 0; x < msg->depth; x++) {
             for (size_t y = 0; y < msg->height; y++) {
                 for (size_t z = 0; z < msg->width; z++) {
                     size_t idx = x * msg->width * msg->height + y * msg->width + z;
                     uint cls = msg->data[idx];
-                    voxblox::GlobalIndex voxelIdx(x + msg->origin_x, y + msg->origin_y, z + msg->origin_z);
-                    //voxblox::SSCOccupancyVoxel* voxel = ssc_map_->getSSCLayerPtr()->getVoxelPtrByGlobalIndex(voxelIdx);
-                    voxblox::SSCOccupancyVoxel * voxel = ssc_map_->getSSCLayerPtr()->getVoxelPtrByGlobalIndex(voxelIdx);
-                    
+                    GlobalIndex voxelIdx(x + msg->origin_x, y + msg->origin_y, z + msg->origin_z);
+                    // voxblox::SSCOccupancyVoxel* voxel =
+                    // ssc_map_->getSSCLayerPtr()->getVoxelPtrByGlobalIndex(voxelIdx);
+                    SSCOccupancyVoxel* voxel = temp_layer.getVoxelPtrByGlobalIndex(voxelIdx);
+
                     // check if the block containing the voxel exists.
                     if (voxel == nullptr) {
-                        //ssc_map_->getSSCLayerPtr()->a
-                        voxblox::BlockIndex block_idx = getBlockIndexFromGlobalVoxelIndex(voxelIdx, 1.0/ ssc_map_->getSSCLayerPtr()->voxels_per_side());
-                        auto block = ssc_map_->getSSCLayerPtr()->allocateBlockPtrByIndex(block_idx);
-                         const VoxelIndex local_voxel_idx =  getLocalFromGlobalVoxelIndex(voxelIdx, ssc_map_->getSSCLayerPtr()->voxels_per_side());
+                        // ssc_map_->getSSCLayerPtr()->a
+                        BlockIndex block_idx =
+                            getBlockIndexFromGlobalVoxelIndex(voxelIdx, 1.0 / temp_layer.voxels_per_side());
+                        auto block = temp_layer.allocateBlockPtrByIndex(block_idx);
+                        const VoxelIndex local_voxel_idx =
+                            getLocalFromGlobalVoxelIndex(voxelIdx, temp_layer.voxels_per_side());
                         voxel = &block->getVoxelByVoxelIndex(local_voxel_idx);
                     }
 
                     if (!voxel->observed) {
                         voxel->label = cls;
-                        voxel->class_confidence = 1.0;// todo - use confidence weight fusion like in SCFusion Paper
+                        voxel->class_confidence = 1.0;  // todo - use confidence weight fusion like in SCFusion Paper
                         voxel->observed = true;
                     }
                 }
             }
         }
+
+        // merge the layer into the map. Used to upsample the predictions
+        mergeLayerAintoLayerB(temp_layer, ssc_map_->getSSCLayerPtr());
     }
 
     std::shared_ptr<SSCMap> ssc_map_;
@@ -116,6 +145,81 @@ std::string SSCMap::Config::print() const {
     // clang-format on
     return ss.str();
 }
+
+// part1 - used during upsampling. just pick largest class
+// labels among voxels. Use default class prediction.
+// like scfusion uses 0.51 for new predictions irespective
+// of probs predicted by the network.
+template <>
+inline SSCOccupancyVoxel Interpolator<SSCOccupancyVoxel>::interpVoxel(const InterpVector& q_vector,
+                                                                      const SSCOccupancyVoxel** voxels) {
+    const int MAX_CLASSES = 12;
+    SSCOccupancyVoxel voxel;
+
+    uint32_t count_observed = 0;
+    std::vector<uint16_t> preds(MAX_CLASSES, 0);
+    for (int i = 0; i < q_vector.size(); ++i) {
+        if (voxels[i]->observed) {
+            count_observed++;
+            preds[voxels[i]->label]++;
+        }
+    }
+
+    // if at least of half of voxels are observed,
+    // assign the maximum class among the voxels
+    if (count_observed > q_vector.size() / 2) {
+        auto max = std::max_element(preds.begin(), preds.end());  
+        auto idx = std::distance(preds.begin(), max);
+        voxel.label = idx;
+        voxel.class_confidence = 1.0f;
+        voxel.observed = true;
+    }
+
+    return voxel;
+}
+
+// part 2 merging upsampled temp layer into voxel of map layer.
+// Note; updated to use log probs and label fusion like in
+// scfusion
+template <>
+void mergeVoxelAIntoVoxelB(const SSCOccupancyVoxel& voxel_A, SSCOccupancyVoxel* voxel_B) {
+    voxel_B->label = voxel_A.label;
+    voxel_B->class_confidence = voxel_A.class_confidence;
+    voxel_B->observed = voxel_A.observed;
+    voxel_B->probability_log = voxel_A.probability_log;
+}
+
+namespace utils {
+    template <>
+    bool isObservedVoxel(const SSCOccupancyVoxel& voxel) {
+    return voxel.observed;
+}
+}
+
+class SSCColorMap : public IdColorMap {
+   public:
+    SSCColorMap() : IdColorMap() {
+        colors_.push_back(Color(22, 191, 206));   // 0 empty, free space
+        colors_.push_back(Color(214, 38, 40));    // 1 ceiling
+        colors_.push_back(Color(43, 160, 4));     // 2 floor
+        colors_.push_back(Color(158, 216, 229));  // 3 wall
+        colors_.push_back(Color(114, 158, 206));  // 4 window
+        colors_.push_back(Color(204, 204, 91));   // 5 chair  new: 180, 220, 90
+        colors_.push_back(Color(255, 186, 119));  // 6 bed
+        colors_.push_back(Color(147, 102, 188));  // 7 sofa
+        colors_.push_back(Color(30, 119, 181));   // 8 table
+        colors_.push_back(Color(188, 188, 33));   // 9 tvs
+        colors_.push_back(Color(255, 127, 12));   // 10 furn
+        colors_.push_back(Color(196, 175, 214));  // 11 objects
+        colors_.push_back(Color(153, 153, 153));  // 12 Accessible area, or label==255, ignore
+    }
+
+    virtual Color colorLookup(const size_t value) const { return colors_[value]; }
+
+   protected:
+    std::vector<Color> colors_;
+};
+
 }  // namespace voxblox
 
 int main(int argc, char **argv) {
@@ -126,7 +230,9 @@ int main(int argc, char **argv) {
     
     //tsdf_server.reset(new voxblox::TsdfServer(nh, nh_private));
     std::unique_ptr<voxblox::SSCServer> ssc_server;
-    ssc_server.reset(new voxblox::SSCServer(nh, nh_private));
+    voxblox::SSCMap::Config config;
+    config.ssc_voxel_size = 0.02; //from the SSC Network which uses 0.02 m as voxel size
+    ssc_server.reset(new voxblox::SSCServer(nh, nh_private, config));
     //ros::Subscriber sub = nh.subscribe("ssc", 1, SSCCallback);
     ros::spin();
     return 0;
