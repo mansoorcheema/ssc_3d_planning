d5310ce1541015e0d756fd03c8520d135ce93a2b
Mansoor <mansoorcheema1@gmail.com>
2021-10-13 14:12:07 +0200

    added cmd option to publish colored eval metrics

--- ssc_mapping/src/ssc_map_eval_node.cpp
+++ ssc_mapping/src/ssc_map_eval_node.cpp
@@ -1,157 +1,5 @@
 
-// general includes
-#include <assert.h>
-#include <glog/logging.h>
-
-#include <fstream>
-#include <iostream>
-
-// ros/pcl includes
-#include <pcl/point_types.h>
-#include <pcl_ros/point_cloud.h>
-#include <ros/ros.h>
-
-// voxblox includes
-#include <ssc_mapping/visualization/visualization.h>
-#include <voxblox/core/block.h>
-#include <voxblox/core/common.h>
-#include <voxblox/core/layer.h>
-#include <voxblox/core/tsdf_map.h>
-#include <voxblox/core/voxel.h>
-#include <voxblox/io/layer_io.h>
-
-using namespace voxblox;
-
-bool isObserved(const voxblox::TsdfVoxel& voxel, float voxel_size) {
-    return voxblox::visualizeOccupiedTsdfVoxels(voxel, voxblox::Point(), voxel_size);
-}
-
-bool isObserved(const voxblox::SSCOccupancyVoxel& voxel, float voxel_size) {
-    voxblox::Color color;
-    return voxblox::visualizeSSCOccupancyVoxels(voxel, voxblox::Point(), &color);
-}
-
-template <typename VoxelTypeA, typename VoxelTypeB>
-void calculate_Intersection_difference(const voxblox::Layer<VoxelTypeA>& layer,
-                                       const voxblox::Layer<VoxelTypeB>& otherLayer, GlobalIndexVector* intersection,
-                                       GlobalIndexVector* difference) {
-    size_t vps = layer.voxels_per_side();
-    size_t num_voxels_per_block = vps * vps * vps;
-
-    voxblox::BlockIndexList blocks;
-    layer.getAllAllocatedBlocks(&blocks);
-    for (const voxblox::BlockIndex& index : blocks) {
-        // Iterate over all voxels in said blocks.
-        const voxblox::Block<voxblox::TsdfVoxel>& block = layer.getBlockByIndex(index);
-
-        for (size_t linear_index = 0; linear_index < num_voxels_per_block; ++linear_index) {
-            // voxblox::Point coord = block.computeCoordinatesFromLinearIndex(linear_index);
-            auto gt_voxel = block.getVoxelByLinearIndex(linear_index);
-
-            if (isObserved(gt_voxel, layer.voxel_size())) {
-                // voxel is observed in first layer. check if it exists in other layer
-
-                // get global voxel index
-                voxblox::VoxelIndex voxel_idx = block.computeVoxelIndexFromLinearIndex(linear_index);
-                voxblox::BlockIndex block_idx = index;
-                voxblox::GlobalIndex global_voxel_idx =
-                    voxblox::getGlobalVoxelIndexFromBlockAndVoxelIndex(block_idx, voxel_idx, layer.voxels_per_side());
-
-                // see if this voxel is observed in other layer
-                auto observed_voxel = otherLayer.getVoxelPtrByGlobalIndex(global_voxel_idx);
-
-                if (observed_voxel != nullptr && isObserved(*observed_voxel, otherLayer.voxel_size())) {
-                    intersection->emplace_back(global_voxel_idx);
-                } else {
-                    difference->emplace_back(global_voxel_idx);
-                }
-            }
-        }
-    }
-}
-
-// Creates a pointcloud from voxel indices
-void createPointCloudFromVoxelIndices(const voxblox::GlobalIndexVector& voxels,
-                                      pcl::PointCloud<pcl::PointXYZRGB>* pointcloud, const voxblox::Color& color, const float voxel_size = 0.08) {
-    for (auto voxel : voxels) {
-        pcl::PointXYZRGB point;
-        point.x = voxel.x() * voxel_size + (voxel_size/2);
-        point.y = voxel.y() * voxel_size + (voxel_size/2);
-        point.z = voxel.z() * voxel_size + (voxel_size/2);
-        point.r = color.r;
-        point.g = color.g;
-        point.b = color.b;
-        pointcloud->push_back(point);
-    }
-    pointcloud->header.frame_id = "world";
-}
-
-// unit_test
-void test_eval_metrics() {
-    voxblox::TsdfMap::Ptr ground_truth_map;
-    voxblox::TsdfMap::Ptr observed_map;
-    voxblox::TsdfMap::Config config;
-    config.tsdf_voxel_size = 1;
-    config.tsdf_voxels_per_side = 8u;
-
-    ground_truth_map = std::make_shared<voxblox::TsdfMap>(config);
-    observed_map = std::make_shared<voxblox::TsdfMap>(config);
-
-    {
-        // add a block at origin
-        voxblox::Point point_in_0_0_0(0, 0, 0);
-        voxblox::Block<voxblox::TsdfVoxel>::Ptr block_0_0_0 =
-            ground_truth_map->getTsdfLayerPtr()->allocateNewBlockByCoordinates(point_in_0_0_0);
-        observed_map->getTsdfLayerPtr()->allocateNewBlockByCoordinates(point_in_0_0_0);
-
-        // add another block
-        voxblox::Point point_in_10_0_0(ground_truth_map->block_size(), 0, 0);
-        voxblox::Block<voxblox::TsdfVoxel>::Ptr block_1_0_0 =
-            ground_truth_map->getTsdfLayerPtr()->allocateNewBlockByCoordinates(point_in_10_0_0);
-
-        {
-            int gt_x_min = 4;
-            int gt_x_max = 12;
-            int gt_y_min = 3;
-            int gt_y_max = 6;
-
-            for (size_t x = gt_x_min; x < gt_x_max; x++) {
-                for (size_t y = gt_y_min; y < gt_y_max; y++) {
-                    // global voxel by global index
-                    // auto voxel =
-                    voxblox::GlobalIndex voxelIdx(x, y, 0);
-
-                    auto voxel = ground_truth_map->getTsdfLayerPtr()->getVoxelPtrByGlobalIndex(voxelIdx);
-                    voxel->weight = 1;
-                }
-            }
-
-            int observed_x_min = 5;
-            int observed_x_max = 8;
-            int observed_y_min = 2;
-            int observed_y_max = 4;
-
-            // add voxels to observed map
-            for (size_t x = observed_x_min; x < observed_x_max; x++) {
-                for (size_t y = observed_y_min; y < observed_y_max; y++) {
-                    // global voxel by global index
-                    // auto voxel =
-                    voxblox::GlobalIndex voxelIdx(x, y, 0);
-
-                    auto voxel = observed_map->getTsdfLayerPtr()->getVoxelPtrByGlobalIndex(voxelIdx);
-                    voxel->weight = 1;
-                }
-            }
-        }
-    }
-
-    voxblox::GlobalIndexVector intersection_gt, difference_gt;
-    calculate_Intersection_difference(ground_truth_map->getTsdfLayer(), observed_map->getTsdfLayer(), &intersection_gt,
-                                      &difference_gt);
-
-    CHECK(intersection_gt.size() == 3) << "Error in Intersection Algorithm! Recheck Implementation.";
-    CHECK(difference_gt.size() == 21) << "Error in DIfference Algorithm! Recheck Implementation.";
-}
+#include "ssc_mapping/eval/map_eval.h"
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "ssc_mapping_eval");
@@ -195,6 +43,21 @@ int main(int argc, char** argv) {
     calculate_Intersection_difference(observed_map->getTsdfLayer(), ground_truth_map->getTsdfLayer(),
                                       &intersection_observed, &difference_observed);
 
+    voxblox::GlobalIndexVector difference_observed_gt, different_unobserved_gt;// Note:here
+    // observed and un observed mean in the voxblox generated observed map,  not in gt
+    splitObservedAndUnObservedVoxels(observed_map->getTsdfLayer(), difference_gt, &difference_observed_gt, &different_unobserved_gt);
+                                      
+    voxblox::GlobalIndexVector free_gt_voxels;
+    calculateFreeObservedVoxels(ground_truth_map->getTsdfLayer(), &free_gt_voxels);
+
+    //voxels that are not observed in observed map but are free space in
+    // ground truth map. In ground truth a free space is either empty voxel or has weight < 1e-6. Thats not the case for observed
+    // layer. Its because voxblox_ground_truth does not fill free space with voxels. There is option to fill but that produces 
+    // false occupied voxels.
+    voxblox::GlobalIndexVector unobserved_unoccupied_voxels;
+    find_unobserved_free_voxels(observed_map->getTsdfLayer(), ground_truth_map->getTsdfLayer(), &unobserved_unoccupied_voxels);
+    
+
     // compute evaluation metrics
     size_t gt_occupied_voxels = 0;
     size_t observed_voxels = 0;
@@ -234,17 +97,30 @@ int main(int argc, char** argv) {
 
     if (argc > 4 && std::string("publish").compare(argv[4]) == 0) {  // publish
 
-        auto missed_occupancy_voxels_pub =
-            nh_private.advertise<pcl::PointCloud<pcl::PointXYZRGB> >("occupancy_pointcloud_diff", 1, true);
+        auto missed_occupancy_observed_voxels_pub =
+            nh_private.advertise<pcl::PointCloud<pcl::PointXYZRGB> >("occupancy_pointcloud_observed_diff", 1, true);
+        auto missed_occupancy_un_observed_voxels_pub =
+            nh_private.advertise<pcl::PointCloud<pcl::PointXYZRGB> >("occupancy_pointcloud_un_observed_diff", 1, true);
         auto correct_occupied_voxels_observed_pub =
             nh_private.advertise<pcl::PointCloud<pcl::PointXYZRGB> >("occupancy_pointcloud_inter", 1, true);
         auto false_positive_observations_pub =
             nh_private.advertise<pcl::PointCloud<pcl::PointXYZRGB> >("false_positive_observations", 1, true);
+        auto free_gt_voxels_pub =
+            nh_private.advertise<pcl::PointCloud<pcl::PointXYZRGB> >("free_gt_voxels", 1, true);
+        auto unobserved_free_voxels_pub =
+            nh_private.advertise<pcl::PointCloud<pcl::PointXYZRGBA> >("unobserved_free_voxels", 1, true);
 
         std::cout << "Publishing voxels comparison!" << std::endl;
-        pcl::PointCloud<pcl::PointXYZRGB> pointcloud_diff;
-        createPointCloudFromVoxelIndices(difference_gt, &pointcloud_diff, Color::Red());
-        missed_occupancy_voxels_pub.publish(pointcloud_diff);
+
+        // publish voxels that are occupied in ground truth but are unoccupied and "observed" in observed map
+        pcl::PointCloud<pcl::PointXYZRGB> pointcloud_observed_diff;
+        createPointCloudFromVoxelIndices(difference_observed_gt, &pointcloud_observed_diff, Color::Red());
+        missed_occupancy_observed_voxels_pub.publish(pointcloud_observed_diff);
+
+        // publish voxels that are occupied in ground truth but are unoccupied and "not observed" in observed map
+        pcl::PointCloud<pcl::PointXYZRGB> pointcloud_un_observed_diff;
+        createPointCloudFromVoxelIndices(different_unobserved_gt, &pointcloud_un_observed_diff, Color::Orange());
+        missed_occupancy_un_observed_voxels_pub.publish(pointcloud_un_observed_diff);
 
         pcl::PointCloud<pcl::PointXYZRGB> pointcloud_inter;
         createPointCloudFromVoxelIndices(intersection_gt, &pointcloud_inter, Color::Green());
@@ -253,6 +129,15 @@ int main(int argc, char** argv) {
         pcl::PointCloud<pcl::PointXYZRGB> pointcloud_observed_occupancy_fp;
         createPointCloudFromVoxelIndices(difference_observed, &pointcloud_observed_occupancy_fp, Color::Yellow());
         false_positive_observations_pub.publish(pointcloud_observed_occupancy_fp);
+        
+        pcl::PointCloud<pcl::PointXYZRGB> pointcloud_free_gt_voxels;
+        createPointCloudFromVoxelIndices(free_gt_voxels, &pointcloud_free_gt_voxels, Color::Gray());
+        free_gt_voxels_pub.publish(pointcloud_free_gt_voxels);
+        // pcl::PointCloud<pcl::PointXYZRGBA> pointcloud_unobserved_free_voxels;
+        // auto color = Color::Gray();
+        // color.a = 80;
+        // createPointCloudFromVoxelIndices(unobserved_unoccupied_voxels, &pointcloud_unobserved_free_voxels, color);
+        // unobserved_free_voxels_pub.publish(pointcloud_unobserved_free_voxels);
 
         ros::spin();
     }
